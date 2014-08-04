ada2e5f5e411e55ccf6b51e96cbeda46b0e2fc24
johnabfaria <abfaria@outlook.com>
2014-08-04 15:41:08 -0500

    Update drop_it.py

--- drop_it.py
+++ drop_it.py
@@ -11,8 +11,7 @@ def upload(name)
 
 	app_key = 'xasdu3mcasb28iq'
 	app_secret = '038cznmg68x1aft'
-	token = '_bg2SsPqgsEAAAAAAAAAEGZBA4gaaF3Oo8UGa8_xnvQSGI3oofN7rFu_WlGUyG3K'
-
+	token =
 	#flow = dropbox.client.DropboxOAuth2FlowRedirect(app_key, app_secret)
 	client = dropbox.client.DropboxClient(token)
 
